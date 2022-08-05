89f75e4a1d0215347a536715c07cb090b360033a
Marvin Andia <mandia1204@gmail.com>
2022-08-04 21:43:48 -0500

    updated output

--- render-templates.py
+++ render-templates.py
@@ -4,6 +4,9 @@ from jinja2 import Environment, FileSystemLoader
 with open('network/vars.yml', 'r') as file:
     vars = yaml.safe_load(file)
 
+for l in vars["subnet_list"]:
+  l['ref'] = f"!Ref {l['id']}"
+
 environment = Environment(loader=FileSystemLoader("network/"))
 network_template = environment.get_template("vpc-and-subnets.j2")
 
